7496be5236cd53a308665b47e11b44c506927e79
Austin Merrick <austinmerrick98@gmail.com>
2020-01-24 15:09:13 -0800

    Initial prototype.

---
+++ iPhoneGame/Assets/playerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public float speed;
+    private float dir;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        dir = Input.GetAxisRaw("Horizontal");
+    }
+
+    void FixedUpdate()
+    {
+        rb.velocity = new Vector2(speed * dir * Time.deltaTime, rb.velocity.y);
+    }
+}
