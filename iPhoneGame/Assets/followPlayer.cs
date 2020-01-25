d55746b0cf4923e3272010b9007ad3b612f9c12c
Austin Merrick <austinmerrick98@gmail.com>
2020-01-24 21:33:52 -0800

    Adding random generation

---
+++ iPhoneGame/Assets/followPlayer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class followPlayer : MonoBehaviour
+{
+    public Transform playerTransform;
+    public float speed = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        if (transform.position.y < playerTransform.position.y)
+            transform.position = new Vector2(transform.position.x, playerTransform.position.y);
+        else
+            transform.position = new Vector2(transform.position.x, transform.position.y+speed*Time.deltaTime);
+    }
+}
---
+++ iPhoneGame/Assets/platformSpawning.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class platformSpawning : MonoBehaviour
+{
+    public Transform cameraTransform;
+    public GameObject platform;
+    public float spawnOffset=8;
+    private float nextPlatformHeight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        nextPlatformHeight = -1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (nextPlatformHeight <= cameraTransform.position.y + spawnOffset)
+        {
+            Instantiate(platform, new Vector2 (Random.Range(-2.5f, 2.5f), nextPlatformHeight), Quaternion.identity);
+            nextPlatformHeight += Random.Range(2f, 3f);
+        }
+    }
+}
