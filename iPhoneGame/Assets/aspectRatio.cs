c107fd03307b7aecc8cb6bb47f3c3471ccf71340
Austin Merrick <austinmerrick98@gmail.com>
2020-01-26 22:07:40 -0800

    Added aspect ratio support and spike sprites

---
+++ iPhoneGame/Assets/aspectRatio.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class aspectRatio : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //get aspect ratio
+        //try to meet 9:16
+        float screenWidthInUnits =  5f * (float)((float)Screen.width / (float)Screen.height);
+        print(screenWidthInUnits);
+        print(Screen.width);
+        print(Screen.height);
+
+        print("testing " + screenWidthInUnits);
+        GetComponent<CinemachineVirtualCamera>().m_Lens.OrthographicSize *= (2.8f / screenWidthInUnits);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
